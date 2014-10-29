c21797d118b18cd27b61e873ec9f519282661946
Pablo Lavegui <pab.gutier@gmail.com>
2014-10-30 00:26:25 +0100

    Caesar_Cypher exercise

---
+++ Week-1-POO/Caesar_Cypher/Caesar_Cypher.rb
@@ -0,0 +1,48 @@
+class Caesar_Cypher
+
+  attr_accessor :string
+
+  def initialize string
+    @string = string
+  end
+
+  def cipher
+    newString = ""
+    (0...@string.length).each do |i|
+      ascii = @string[i].ord + 3
+      if ascii > 122
+        ascii -= 26
+      end
+      newString << ascii.chr
+
+    end
+    newString
+  end
+
+  def cipherDeco
+    newString = ""
+    (0...@string.length).each do |i|
+    #string.each_char
+      ascii = @string[i].ord - 3
+      if ascii < 97
+        ascii += 26
+      end
+      newString << ascii.chr
+
+    end
+    newString
+  end
+
+end
+
+caesar = Caesar_Cypher.new("abc")
+
+puts "@string: #{caesar.string}"
+puts caesar.cipher
+puts caesar.cipherDeco
+
+caesar.string = "zxy"
+puts "@string: #{caesar.string}"
+
+puts caesar.cipher
+puts caesar.cipherDeco
