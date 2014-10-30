10949e9b94087d4a2cda3e786b8cbe91493ee071
Pablo Lavegui <pab.gutier@gmail.com>
2014-10-30 17:29:56 +0100

    Keynote file read

--- Week-1-POO/console-keynote/File_Read.rb
+++ Week-1-POO/console-keynote/File_Read.rb
@@ -1,6 +1,21 @@
-class FileRead
+class File_Read
 
-  def initialize
+  def initialize name
+    @name       = name
+    @all_slides = []
+  end
+
+  def slides
+    File.open(@name) do |f|
+
+      while line = f.gets
+        unless line.include? '----'
+          @all_slides.push line.chomp
+        end
+      end
+
+      @all_slides
+    end
   end
 
 end
--- Week-1-POO/console-keynote/Keynote.rb
+++ Week-1-POO/console-keynote/Keynote.rb
@@ -1,10 +1,18 @@
+require './File_Read'
+
 class Keynote
 
   def initialize
+    @slides
+  end
+
+  def load_keynote_file name
+    @slides = File_Read.new(name).slides
+    puts @slides
   end
 
 end
 
 keynote = Keynote.new
-keynote.load_file 'slides.txt'
-keynote.play
+keynote.load_keynote_file 'slides.txt'
+#keynote.play
