7719ee0b646efa9f2436fadbbe1cf1a98f9c7405
Pablo Lavegui <pab.gutier@gmail.com>
2014-10-30 17:51:10 +0100

    Keynote move

--- Week-1-POO/console-keynote/Keynote.rb
+++ Week-1-POO/console-keynote/Keynote.rb
@@ -4,15 +4,42 @@ class Keynote
 
   def initialize
     @slides
+    @actual_slide = 0
   end
 
   def load_keynote_file name
     @slides = File_Read.new(name).slides
-    puts @slides
+  end
+
+  def start
+    @actual_slide = 0
+    view_actual_slide
+  end
+
+  def next
+    @actual_slide += 1
+    if @actual_slide > @slides.size - 1
+      @actual_slide = @slides.size - 1
+    end
+    view_actual_slide
+  end
+
+  def previous
+    @actual_slide -= 1
+    if @actual_slide < 0
+      @actual_slide = 0
+    end
+    view_actual_slide
+  end
+
+  def view_actual_slide
+    puts @slides[@actual_slide]
   end
 
 end
 
 keynote = Keynote.new
 keynote.load_keynote_file 'slides.txt'
-#keynote.play
+keynote.start
+6.times{ keynote.next }
+keynote.previous
