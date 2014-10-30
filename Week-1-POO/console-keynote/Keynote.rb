59f9d1660b45745de9b5b861eaa64b4ebe5439ee
Pablo Lavegui <pab.gutier@gmail.com>
2014-10-30 18:41:00 +0100

    Keynote Slide_Print

--- Week-1-POO/console-keynote/Keynote.rb
+++ Week-1-POO/console-keynote/Keynote.rb
@@ -1,4 +1,5 @@
 require './File_Read'
+require './Slide_Print'
 
 class Keynote
 
@@ -18,22 +19,26 @@ class Keynote
 
   def next
     @actual_slide += 1
-    if @actual_slide > @slides.size - 1
+    unless @actual_slide > @slides.size - 1
+      view_actual_slide
+    else
       @actual_slide = @slides.size - 1
     end
-    view_actual_slide
   end
 
   def previous
     @actual_slide -= 1
-    if @actual_slide < 0
+    unless @actual_slide < 0
+      view_actual_slide
+    else
       @actual_slide = 0
     end
-    view_actual_slide
   end
 
   def view_actual_slide
-    puts @slides[@actual_slide]
+    #puts @slides[@actual_slide]
+    screen = Slide_Print.new(@slides[@actual_slide])
+    screen.show
   end
 
 end
--- Week-1-POO/console-keynote/Slide.rb
+++
@@ -1,6 +0,0 @@
-class Slide
-
-  def initialize
-  end
-
-end
