4df479c7ffb5f357f416343930d43879e761cf01
Pablo Lavegui <pab.gutier@gmail.com>
2014-11-04 16:07:35 +0100

    Add cents

--- Week-2/check-spell-out/CheckSpellOut.rb
+++ Week-2/check-spell-out/CheckSpellOut.rb
@@ -14,23 +14,40 @@ class CheckSpellOut
 
   def spell num
     str = ""
+    str << integer_part(num.to_i)
+    str << decimal_string(num)
+    str << " dollars"
+  end
+
+  def integer_part num
+    str = ""
     if num > 99
       str << spell_cents(num)
-      str << spell(num % 100) if num % 100 > 0
+      str << integer_part(num % 100) if !is_mult?(num, 100)
     elsif num > 19
       str << spell_dec(num)
-      str << spell(num % 10) if num % 10 > 0
+      str << integer_part(num % 10) if !is_mult?(num, 10)
     else
       str << @zero_to_nineteen[num]
     end
     str
   end
 
+  def decimal_string num
+    if num.class == Float
+      decimal_str =
+        " and #{(((num - num.to_i) * 100).to_i).to_s.rjust(2, '0')} cents"
+    else
+      decimal_str = ""
+    end
+    decimal_str
+  end
+
   def spell_cents num
     str = ""
     str << @zero_to_nineteen[num / 100]
     str << " hundred"
-    if num % 100 > 0
+    if !is_mult?(num, 100)
       str << " and "
     end
     str
@@ -39,10 +56,14 @@ class CheckSpellOut
   def spell_dec num
     str = ""
     str << @twelve_to_ninety[num / 10]
-    if num % 10 > 0
+    if !is_mult?(num, 10)
       str << "-"
     end
     str
+  end
+
+  def is_mult? a, b
+    a % b == 0
   end
 
 end
