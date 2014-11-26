599e5d5ff8ce896ad981d5f371ac973734af66e5
pab.gutier@gmail.com <pab.gutier@gmail.com>
2014-11-26 12:31:34 +0100

    CSS exercises

--- Week-5-JQuery-CSS/jquery/public/ajax.js
+++ Week-5-JQuery-CSS/jquery/public/ajax.js
@@ -1,33 +1,33 @@
 // Replace myContent content with the result of the request google.html
-$("#myContent").load('google.html');
+//$("#myContent").load('google.html');
 
 // Replace the result div when click on it with the response of submiting the name to the /hello
-$("#result").click(function() {
-  data = $("#name").val();
-  $.get("/hello/" + data, function(data) {
-    $("#result").text(data);
-  });
-});
+//$("#result").click(function() {
+  //data = $("#name").val();
+  //$.get("/hello/" + data, function(data) {
+    //$("#result").text(data);
+  //});
+//});
 
 // Replace the result div when click on it when the user changes the amount or the currency
 // path: /exchange with the parameters amount and currency
-$("#result").click(function() {
-  var options = {
-    "amount":   $("#amount").val(),
-    "currency": $("#currency").val()
-  };
-  $.get("/exchange/", options, function(data) {
-    console.log(data);
-  });
-});
+//$("#result").click(function() {
+  //var options = {
+    //"amount":   $("#amount").val(),
+    //"currency": $("#currency").val()
+  //};
+  //$.get("/exchange/", options, function(data) {
+    //console.log(data);
+  //});
+//});
 
 // Save (POST) the personal form to the /add route when click on the result div
 // Look the the AJAX Response and the status code
-$("#result").click(function() {
-  $.post('/add', $("#personal").serialize(), function(data) {
-    console.log(data);
-  });
-});
+//$("#result").click(function() {
+  //$.post('/add', $("#personal").serialize(), function(data) {
+    //console.log(data);
+  //});
+//});
 
 // Make a get request to /sleep when click on the result div
 // Show loading while the response does not arrive
@@ -38,7 +38,7 @@ $("#result").click(function() {
   console.log("loading....");
   $.ajax("/sleep", {
     success:  function() {
-      console.log('Succes');
+      console.log('Success');
     },
 
     complete: function() {
