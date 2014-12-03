62955b99c6c247ca4a266d5bd21cdc87e4be7cce
pab.gutier@gmail.com <pab.gutier@gmail.com>
2014-12-03 11:55:56 +0100

    Add has_many likes and dislikes

--- Week-6-Rails/manderley_cast/app/models/movie.rb
+++ Week-6-Rails/manderley_cast/app/models/movie.rb
@@ -5,15 +5,14 @@ class Movie < ActiveRecord::Base
     has_many :people, through: :casts
 
     has_many :votes
+    has_many :likes, 
+               ->() { likes },
+               class_name: 'Vote'
 
+    has_many :dislikes, 
+               ->() { dislikes },
+               class_name: 'Vote'
 
-    def likes
-      votes.likes      # likes es el scope
-    end
-
-    def dislikes
-      votes.dislikes
-    end
 
     validates_presence_of :title
     validates_uniqueness_of :title
